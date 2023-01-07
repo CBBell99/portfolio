4dbcadbdf7b319829013c45b1a34d10352d9e947
CBBell99 <chrisbeckertonbell@gmail.com>
2023-01-06 16:39:42 -0800

    basic structure complete

---
+++ src/components/Intro.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Intro() {
+  return (
+    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+      <h1>Chris</h1>
+      <p>Full-Stack Web Developer</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel ut quos
+        deleniti inventore consequuntur sint esse veniam a architecto quod
+        perspiciatis et asperiores temporibus hic, necessitatibus impedit. Quos,
+        reiciendis esse!
+      </p>
+    </div>
+  );
+}
+
+export default Intro;
