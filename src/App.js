db5d52ca833eacfc3913782ab64417118a60fdce
CBBell99 <chrisbeckertonbell@gmail.com>
2023-01-07 15:36:44 -0800

    barebones of portfolio

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
-import "./App.css";
-import Intro from "./components/Intro";
-import Portfolio from "./components/Portfolio";
-import Timeline from "./components/Timeline";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillInstagram,
+} from "react-icons/ai";
 
 function App() {
   return (
-    <>
-      <Intro />
-      <Portfolio />
-      <Timeline />
-      <Contact />
-      <Footer />
-    </>
+    <div className="bg-white px-10 ">
+      <section className="min-h-screen">
+        <nav className="py-10 mb-12 flex justify-between">
+          <h1 className="text-xl">Chris Bell</h1>
+          <ul className="flex items-center">
+            <li>
+              <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
+            </li>
+            <li>
+              <a
+                className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                href="https://resume.creddle.io/resume/20aimu9xvnb"
+              >
+                Resume
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="text-center p-10">
+          <h2 className="text-5xl py-2 text-teal-600 font-medium">
+            Chris Bell
+          </h2>
+          <h3 className="text-2xl py-2">Full-Stack Web Developer</h3>
+          <p className="text-medium py-5 leading-8 text-gray-800">
+            Hi I'm Chris. I build full stack web apps that you will love to use.
+          </p>
+        </div>
+        <div>
+          <AiFillTwitterCircle />
+          <AiFillLinkedin />
+          <AiFillInstagram />
+        </div>
+      </section>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{jsx,js}"],
-  darkmode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{jsx,js}",
+    "./components/**.*.{js,ts,jsx.tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
