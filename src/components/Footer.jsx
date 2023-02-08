43b402edf66320f4eee3d67c94342205acbb2f66
CBBell99 <chrisbeckertonbell@gmail.com>
2023-02-08 14:12:46 -0800

    create footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 function Footer() {
-  return <div>Footer</div>;
+
+const year = new Date().getFullYear();
+console.log(year);
+
+return (
+  <footer className="h-24 p-4 md:px-14 ">
+    <div className="border-t h-full border-t-gray-600 flex justify-between items-end">
+      <p className="text-greyLight">created by Chris Bell</p>
+      <p className="text-greyLight">
+        all rights reserved <span>&copy;</span> {year} Chris Bell
+      </p>
+    </div>
+  </footer>
+);
 }
 
 export default Footer;
